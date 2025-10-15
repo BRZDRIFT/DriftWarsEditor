b51ce7947fbb1b2acedd6365cf34c1cbd47de273
unknown <oparowski90@gmail.com>
2025-10-15 06:28:43 -0700

    stuff

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 def define_env(env):
 
+    baseUrl = ''
+
+    bGitHub =  env.variables.get('GITHUB_ACTIONS', False)
+    if bGitHub:
+        baseUrl = '/DriftWarsEditor'
+
     @env.macro
     def fn(fnName):
-        baseUrl = env.variables.get('base_url', '')
         return '[' + fnName +']('+baseUrl+'/driftScript/functions/#' + fnName.lower() +')'
 
     @env.macro
     def enum(enumName):
-        baseUrl = env.variables.get('base_url', '')
         return '[' + enumName +']('+baseUrl+'/driftScript/enumerations/#' + enumName.lower() +')'
 
     @env.macro
     def entry(entryName):
-        baseUrl = env.variables.get('base_url', '')
         return '[' + entryName +']('+baseUrl+'/driftScript/scriptEntryPoints/#' + entryName.lower() +')'
 
     @env.macro
     def eventQueue():
-        baseUrl = env.variables.get('base_url', '')
         return '[Event Queue]('+baseUrl+'/driftScript/eventQueue/#event_queue)'
 
     @env.macro
     def type(typeName):
-        baseUrl = env.variables.get('base_url', '')
         return '[' + typeName + ']('+baseUrl+'/driftScript/builtinTypes/#'+typeName.lower()+')'
